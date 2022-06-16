491a40db7145b398712079b0a6bfcf9e17875031
DanZhun <dan861202@gmail.com>
2022-06-16 22:08:40 +0800

    resume

---
+++ js/add-contents.js
@@ -0,0 +1,31 @@
+// add-contents 
+let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+
+// alert('Nice to meet you.');
+
+if (hourNow > 18){
+    greeting ='Good Night';
+}
+else if (hourNow > 12){
+    greeting ='Good Afternoon';
+}
+else if (hourNow > 0){
+    greeting ='Good Morning';
+}
+else {
+    greeting ='Welcome';
+}
+
+document.write('<h3>' + greeting + '</h3>' + 'Neet to meet you');
+
+//Date floor
+$(document).ready(function() {
+
+   let today = new Date();
+   let year = today.getFullYear();
+  
+    $('#footer').html('<p>Copyright &copy;' + year + ' ' + 'Dan Wu</p>');
+  
+  });
