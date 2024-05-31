ba823ca366abeaafe4cfcc2da18160223e5f2a92
DanZhun <dan861202@gmail.com>
2024-05-31 15:11:18 +0800

    0531 edit three
    very damn.

--- docs/js/vue.js
+++ docs/js/vue.js
@@ -21,7 +21,7 @@
             birthday : '1997/12/02',
             blood : 'A型',
             school : '彰化師大資管系數科組 碩二 (就讀中)',
-            license : ['乙級-電腦硬體裝修', 'TQC-World(2016)', 'TQC+程式語言(Python)'],
+            license : ['乙級-電腦硬體裝修', 'TQC-World(2016)', 'TQC+程式語言(Python)',],
         //Study experience    
             study : [ '●朝陽科大-資管系(畢業)','●國立埔里高級工業職業學校-微電腦修護科(畢業)'],
             currentjob : ['●彰化師大-資管系數科組 碩士班'],
@@ -31,7 +31,7 @@
             work2 : '百佳泰股份有限公司-測試工程師(實習生)',
             workexp2 : ['●更加瞭解科技產業', '●設備上的故障排除', '●測試硬體設備'],
         //activity experience
-            activity : '●朝陽科大校隊男排',
+            activity : '●朝陽科大校隊男排 (110)',
             // footer
             copyright : '版權所有 翻印必究',
             company: 'Dan Wu',
